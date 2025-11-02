2cc1ebf08dd2aeab6969b5f2f9e0f0a3fbe80d14
luana-fs <lua.f@hotmail.com>
2025-11-02 19:01:38 -0300

    testeeee

--- infra/lambda.tf
+++ infra/lambda.tf
@@ -1,18 +1,13 @@
 resource "aws_lambda_function" "spring_lambda" {
   function_name = "lambda-cs-t1-springboot"
   package_type  = "Image"
-  image_uri = "public.ecr.aws/lambda/java:17-al2"
+  image_uri = "843483113908.dkr.ecr.us-east-1.amazonaws.com/cs-t1-springboot-repo:latest"
   timeout       = 30
   memory_size   = 1024
   role          = "arn:aws:iam::843483113908:role/LabRole"
 
-  vpc_config {
-    # Usa o ID do novo security group
-    security_group_ids = [aws_security_group.lambda_sg.id]
-    
-    # Usa todas as subnets da sua VPC Padrão
-    subnet_ids         = data.aws_subnets.default.ids
-  }
+  architectures = ["x86_64"]
+
   lifecycle {
     ignore_changes = [
       image_uri,
--- infra/security_groups.tf
+++ infra/security_groups.tf
@@ -1,22 +1,23 @@
-# Define um Security Group para o RDS
 resource "aws_security_group" "rds_sg" {
   name        = "rds-postgres-sg"
-  description = "Allow inbound traffic for RDS Postgres"
-  vpc_id      = data.aws_vpc.default.id # Usa a VPC Padrão existente
+  description = "Allow inbound traffic from Lambda to RDS"
+  vpc_id      = data.aws_vpc.default.id 
 
-  # Exemplo de regra de entrada (inbound)
   ingress {
-    from_port   = 5432
-    to_port     = 5432
-    protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/16"] # Permite tráfego da sua VPC
+    from_port = 5432
+    to_port   = 5432
+    protocol  = "tcp"
+    
+    # Substitui 'cidr_blocks' por 'security_groups'
+    security_groups = [aws_security_group.lambda_sg.id] 
   }
 
+  # Permite que o RDS envie tráfego de VOLTA para a Lambda
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = [data.aws_vpc.default.cidr_block] # Permite tráfego da VPC Padrão
+    cidr_blocks = ["0.0.0.0/0"] # Pode ser "0.0.0.0/0" ou o CIDR da VPC
   }
 
   tags = {
@@ -26,15 +27,22 @@ resource "aws_security_group" "rds_sg" {
 
 resource "aws_security_group" "lambda_sg" {
   name        = "lambda-vpc-sg"
-  description = "Security group for Lambda function"
+  description = "Security group for Lambda function and VPC Endpoints"
   vpc_id      = data.aws_vpc.default.id
 
-  # Permite que a Lambda aceda à Internet (necessário para o ECR, logs, etc.)
+  ingress {
+    from_port = 443
+    to_port   = 443
+    protocol  = "tcp"
+    self      = true # Permite tráfego vindo do próprio grupo
+  }
+
+  # Permite que a Lambda acesse a internet/Endpoints na porta 443
   egress {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # Permite sair para qualquer IP
+    cidr_blocks = ["0.0.0.0/0"] 
   }
 
   tags = {
