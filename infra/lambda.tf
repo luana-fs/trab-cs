9a90ff29131a847fee877f580f1bd041a473f4b5
luana-fs <lua.f@hotmail.com>
2025-11-02 16:18:43 -0300

    ajuste imagem lambda

--- infra/lambda.tf
+++ infra/lambda.tf
@@ -1,6 +1,7 @@
 resource "aws_lambda_function" "spring_lambda" {
   function_name = "lambda-cs-t1-springboot"
   package_type  = "Image"
+  image_uri = "public.ecr.aws/lambda/java:17"
   timeout       = 30
   memory_size   = 1024
   role          = "arn:aws:iam::843483113908:role/LabRole"
@@ -11,6 +12,12 @@ resource "aws_lambda_function" "spring_lambda" {
     
     # Usa todas as subnets da sua VPC Padrão
     subnet_ids         = data.aws_subnets.default.ids
+  }
+  lifecycle {
+    ignore_changes = [
+      image_uri,
+      # last_modified, 
+    ]
   }
 }
 
