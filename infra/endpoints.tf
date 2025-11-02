f4ad96585744f87f73279c80f4fb03b4ed292747
luana-fs <lua.f@hotmail.com>
2025-11-02 16:23:57 -0300

    ajuste vpc

---
+++ infra/endpoints.tf
@@ -0,0 +1,39 @@
+# Precisamos saber qual a VPC que estamos usando
+data "aws_vpc" "default" {
+  default = true
+}
+
+# 1. Endpoint para a API do ECR (necessário para "docker login", etc.)
+resource "aws_vpc_endpoint" "ecr_api" {
+  vpc_id              = data.aws_vpc.default.id
+  service_name        = "com.amazonaws.${var.aws_region}.ecr.api" # 'var.aws_region' deve ser sua variável de região
+  vpc_endpoint_type   = "Interface"
+  
+  # Associa às mesmas subnets da Lambda
+  subnet_ids          = data.aws_subnets.default.ids 
+  
+  # Associa ao mesmo Security Group da Lambda
+  security_group_ids  = [aws_security_group.lambda_sg.id] 
+  private_dns_enabled = true
+}
+
+# 2. Endpoint para o "Docker" do ECR (necessário para "docker pull/push")
+resource "aws_vpc_endpoint" "ecr_dkr" {
+  vpc_id              = data.aws_vpc.default.id
+  service_name        = "com.amazonaws.${var.aws_region}.ecr.dkr"
+  vpc_endpoint_type   = "Interface"
+  
+  subnet_ids          = data.aws_subnets.default.ids
+  security_group_ids  = [aws_security_group.lambda_sg.id]
+  private_dns_enabled = true
+}
+
+# 3. Endpoint para o S3 (necessário porque as camadas do ECR são armazenadas no S3)
+resource "aws_vpc_endpoint" "s3" {
+  vpc_id            = data.aws_vpc.default.id
+  service_name      = "com.amazonaws.${var.aws_region}.s3"
+  vpc_endpoint_type = "Gateway"
+  
+  # Endpoints do tipo Gateway se anexam à tabela de rotas da VPC
+  route_table_ids = data.aws_vpc.default.main_route_table_id
+}
--- infra/lambda.tf
+++ infra/lambda.tf
@@ -1,7 +1,7 @@
 resource "aws_lambda_function" "spring_lambda" {
   function_name = "lambda-cs-t1-springboot"
   package_type  = "Image"
-  image_uri = "public.ecr.aws/lambda/java:17"
+  image_uri = "public.ecr.aws/lambda/java:17-al2"
   timeout       = 30
   memory_size   = 1024
   role          = "arn:aws:iam::843483113908:role/LabRole"
