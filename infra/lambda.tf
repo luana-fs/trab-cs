f0832cd005b3d1cd013c89e5f9dabb38a2e2513b
luana-fs <lua.f@hotmail.com>
2025-11-02 16:14:13 -0300

    remoção da tag image

--- infra/lambda.tf
+++ infra/lambda.tf
@@ -1,7 +1,6 @@
 resource "aws_lambda_function" "spring_lambda" {
   function_name = "lambda-cs-t1-springboot"
   package_type  = "Image"
-  image_uri     = "${aws_ecr_repository.lambda_repo.repository_url}:latest"
   timeout       = 30
   memory_size   = 1024
   role          = "arn:aws:iam::843483113908:role/LabRole"
